43fcf52ba31a6488aa5d066f8f229b248f974120
wjingsin <wongjingsin123@gmail.com>
2025-08-15 15:23:09 +0800

    Add support for Add, List

--- src/main/java/Ember.java
+++ src/main/java/Ember.java
@@ -1,8 +1,11 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Ember {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int currentListNumber = 0;
         String line = "____________________________________________________________\n";
         System.out.println(line +
                 "Hello! I'm Ember\n" +
@@ -17,8 +20,22 @@ public class Ember {
                         line
                 );
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                int i = 0;
+                System.out.println(line);
+                while (i < list.length && list[i] != null) {
+                    System.out.println(
+                            (i + 1) +
+                            ". " +
+                            list[i]);
+                    i++;
+                }
+                System.out.println(line);
             } else {
+                list[currentListNumber] = userInput;
+                currentListNumber++;
                 System.out.println(line +
+                        "added: " +
                         userInput +
                         "\n" +
                         line
