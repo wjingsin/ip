fb84882698ebcbeb11eee4d45882bf1cd96fd2cd
wjingsin <wongjingsin123@gmail.com>
2025-08-15 14:34:54 +0800

    Add greeting

--- src/main/java/Ember.java
+++ src/main/java/Ember.java
@@ -1,4 +1,11 @@
 public class Ember {
     public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        System.out.println(line +
+                "Hello! I'm Ember\n" +
+                "What can I do for you?\n" +
+                line +
+                "Bye. Hope to see you again soon!\n" +
+                line);
     }
 }
