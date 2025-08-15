6ed3422d260746935b7bbe22843e0f80e8e615fd
wjingsin <wongjingsin123@gmail.com>
2025-08-15 14:52:04 +0800

    Add support for echo

--- src/main/java/Ember.java
+++ src/main/java/Ember.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
+
 public class Ember {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String line = "____________________________________________________________\n";
         System.out.println(line +
                 "Hello! I'm Ember\n" +
                 "What can I do for you?\n" +
-                line +
-                "Bye. Hope to see you again soon!\n" +
-                line);
+                line
+        );
+        while(true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println(line +
+                        "Bye. Hope to see you again soon!\n" +
+                        line
+                );
+                break;
+            } else {
+                System.out.println(line +
+                        userInput +
+                        "\n" +
+                        line
+                );
+            }
+        }
     }
 }
